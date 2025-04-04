8d677ec38ec8c3ffaa80f16036b863bfa9805c82
Kazuya Yokoyama <yk3kzy@gmail.com>
2025-04-05 08:07:10 +0900

    TagName に変更

--- go/shared/models/tag.go
+++ go/shared/models/tag.go
@@ -1,6 +1,6 @@
 package models
 
 type Tag struct {
-	Name      string `json:"name" dynamodbav:"name"`
+	TagName      string `json:"tag_name" dynamodbav:"tag_name"`
 	CreatedAt string `json:"createdAt" dynamodbav:"createdAt"`
 }
